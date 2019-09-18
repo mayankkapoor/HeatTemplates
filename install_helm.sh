9cd883f508b154a9453c230569241a0512f494ec
Mohamedji Bardolia <6219755+Mohamedji@users.noreply.github.com>
2019-09-18 13:04:12 +0530

    Create install_helm.sh

---
+++ install_helm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source /root/proxy
+wget https://get.helm.sh/helm-v2.14.3-linux-amd64.tar.gz
+tar -zxvf helm-v2.14.3-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin
+
+helm init
+kubectl create serviceaccount --namespace kube-system tiller
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
